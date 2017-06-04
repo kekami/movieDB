dc6346a654c302fc900d7e8064d015295e0a54db
Kevin Kamimura <kevin.kamimura@outlook.com>
2017-06-04 20:33:35 +0200

    Updated console output to reflect the port that is being used.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });

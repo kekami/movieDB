0e728b5ad1e3b07949b7b4bb832a277995156113
Kevin Kamimura <kekami@users.noreply.github.com>
2017-06-06 08:59:31 -0700

    Removed redux devtools integration

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,5 +1,5 @@
 import 'babel-polyfill';
-import { combineReducers, applyMiddleware, createStore, compose } from 'redux';
+import { combineReducers, applyMiddleware, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { reducer as form } from 'redux-form';
 import { client, signup, login, movie, search, like } from '../reducers';
@@ -8,12 +8,6 @@ import { SignupSaga, LoginSaga, MovieSaga, SearchSaga, LikeSaga } from '../sagas
 const sagaMiddleware = createSagaMiddleware();
 
 let store;
-
-/*eslint-disable */
-const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
-/*eslint-enable */
 
 function* IndexSaga() {
   yield [
